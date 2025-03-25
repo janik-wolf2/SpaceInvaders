990a3f3c08701a1705a56cf447e338007e727115
mjaniszewski <mjaniszewski@S3005>
2025-03-25 14:42:23 +0100

    no elo elo

--- SpaceInvaders/strony/planszaGry.xaml.cs
+++ SpaceInvaders/strony/planszaGry.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace SpaceInvaders.strony
@@ -21,12 +22,42 @@ namespace SpaceInvaders.strony
     /// </summary>
     public partial class planszaGry : Page
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int bulletTimer = 0;
+        int liczbaZyc = 3;
+        bool goLeft = false;
+        bool goRight = false;
+        bool strzlaj = false;
+
+
         public planszaGry()
         {
             InitializeComponent();
             statek.Source = new BitmapImage(new Uri("/resources/statek.png", UriKind.Relative));
-            MessageBox.Show("xdd");
+            planszaCanvas.Focus();
+
+            timer.Tick += petlaGry;
+            timer.Interval = TimeSpan.FromMicroseconds(1000);
+            timer.Start();
         }
+
+        private void petlaGry(object? sender, EventArgs e)
+        {
+            foreach(var x in planszaCanvas.Children.OfType<Rectangle>())
+            {
+                if(x is Rectangle && (string)x.Tag == "pocisk")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) - 20);
+                }
+            }
+
+
+
+
+            
+        }
+
+        
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
@@ -38,17 +69,44 @@ namespace SpaceInvaders.strony
 
         public void HandleKeyPress(KeyEventArgs e)
         {
-            Thickness currentMargin = statek.Margin;
+            
 
-            if (e.Key == Key.Right && currentMargin.Left < 778)
+            if (e.Key == Key.Left && Canvas.GetLeft(statek) >= 0)
             {
-                statek.Margin = new Thickness(currentMargin.Left + 20, currentMargin.Top, currentMargin.Right, currentMargin.Bottom);
+
+                //statek.Margin = new Thickness(currentMargin.Left + 20, currentMargin.Top, currentMargin.Right, currentMargin.Bottom);
+                Canvas.SetLeft(statek, Canvas.GetLeft(statek) - 20);
             }
 
-            if (e.Key == Key.Left && currentMargin.Left > 0)
+            if (e.Key == Key.Right && Canvas.GetLeft(statek) < 770)
             {
-                statek.Margin = new Thickness(currentMargin.Left - 20, currentMargin.Top, currentMargin.Right, currentMargin.Bottom);
+
+                //statek.Margin = new Thickness(currentMargin.Left + 20, currentMargin.Top, currentMargin.Right, currentMargin.Bottom);
+                Canvas.SetLeft(statek, Canvas.GetLeft(statek) + 20);
             }
+
+
+            if (e.Key == Key.Space)
+            {
+                Rectangle pociskGracza = new Rectangle
+                {
+                    Tag = "pocisk",
+                    Width = 5,
+                    Height = 25,
+                    Fill = Brushes.Red
+                };
+
+
+                Canvas.SetTop(pociskGracza, Canvas.GetTop(statek) - pociskGracza.Height);
+                Canvas.SetLeft(pociskGracza, (Canvas.GetLeft(statek) - pociskGracza.Width / 2) + 35);
+
+                planszaCanvas.Children.Add(pociskGracza);
+            }
+
+            goRight = false;
+            goLeft = false;
+
+
         }
     }
 }
